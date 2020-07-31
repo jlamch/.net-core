a977d09b0579d2a5c2a887c93d470e6a6fb63725
Joanna Lamch <jlamch@gmail.com>
2020-07-31 21:55:12 +0000

    convert by additional properties

--- grpc-example/GrpcExampleService/GrpcExampleClient/Quize.cs
+++ grpc-example/GrpcExampleService/GrpcExampleClient/Quize.cs
@@ -7,6 +7,7 @@ namespace GrpcExampleService
     public long Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-      public List<Question> Questions { get; set; }
+    public List<Question> Questions { get; set; }
+    public IDictionary<bool,string> Answers { get; set; }
   }
 }
