c22727959a7e8632d871e3fd6cad29a505f937ed
Joanna Lamch <jlamch@gmail.com>
2020-09-08 21:20:29 +0000

    graphQL 1

---
+++ graphql-example/GraphQlExample/GraphQlExample/Data/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphQlExample.Data
+{
+  public class Answer
+  {
+    public long Id { get; set; }
+    public Question Question { get; set; }
+    public string Text { get; set; }
+    public bool? IsCorrect { get; set; }
+  }
+}
---
+++ graphql-example/GraphQlExample/GraphQlExample/Data/Question.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQlExample.Data
+{
+  public class Question
+  {
+  }
+}
